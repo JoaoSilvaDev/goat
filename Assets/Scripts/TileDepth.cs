11901f635536e314093bb10af2e46c326366627f
Joao Silva <joaosdev@gmail.com>
2018-12-02 00:33:20 +0000

    Imported tiles and auto tile depth sorter

---
+++ Assets/Scripts/TileDepth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileDepth : MonoBehaviour
+{
+	SpriteRenderer[] tiles;
+
+	void Start()
+	{
+		tiles = GetComponentsInChildren<SpriteRenderer>();
+		
+		foreach(SpriteRenderer tile in tiles)
+			tile.sortingOrder = Mathf.RoundToInt(tile.transform.position.y) * -100;
+	}
+}
