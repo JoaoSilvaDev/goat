99b9708175474c9dd55f66c82d9f8a92915907da
Joao Silva <joaosdev@gmail.com>
2018-12-02 01:17:20 +0000

    enemy
    enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+	int currentPoint = 0;
+	Vector3 targetPos = Vector3.zero;
+	List<Vector3>[] points = new List<Vector3>();
+
+	void Start()
+	{
+		targetPos = transform.position;
+	}
+	void Move()
+	{}
+
+	void NextPoint()
+	{
+		currentPoint++;
+	}
+
+	
+}
