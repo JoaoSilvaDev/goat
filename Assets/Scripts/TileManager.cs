4c968875071b1be2cd2219d5ac21995a8344e611
Ivo Penedo <ivoti.98@sapo.pt>
2018-12-03 17:00:27 +0000

    Tile manager not initiated

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class TileDepth : MonoBehaviour
+public class TileManager : MonoBehaviour
 {
 	SpriteRenderer[] tiles;
 
