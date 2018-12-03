b1aefdbf1e145159b539f48e36a1aa6bdc037247
Joao Silva <joaosdev@gmail.com>
2018-12-03 20:56:42 +0000

    more levels

--- Assets/GroundRandomTile.cs
+++ Assets/GroundRandomTile.cs
@@ -6,14 +6,14 @@ using UnityEngine.SceneManagement;
 public class GroundRandomTile : MonoBehaviour
 {
     SpriteRenderer sr;
-    public Sprite[] variations = new Sprite[4];
+    public Sprite[] variations = new Sprite[3];
 
     private void Start()
     {
         sr = GetComponent<SpriteRenderer>();
 
         int sceneIndex = SceneManager.GetActiveScene().buildIndex;
-        float rand = Mathf.PerlinNoise(transform.position.x + 7 * sceneIndex, transform.position.y) * 10 - 2;
+        float rand = Mathf.PerlinNoise(transform.position.x + 7 * sceneIndex, transform.position.y) * 10 - 3;
         int index = (int)rand;
         Debug.Log(index);
         sr.sprite = variations[index];
