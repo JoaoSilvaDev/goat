c3b34953a1918c5ae75231141abe5a2e8f12e726
Joao Silva <joaosdev@gmail.com>
2018-12-03 17:57:22 +0000

    Tile Randomization

---
+++ Assets/GroundRandomTile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GroundRandomTile : MonoBehaviour
+{
+    SpriteRenderer sr;
+    public Sprite[] variations = new Sprite[4];
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        float rand = Mathf.PerlinNoise(transform.position.x * sceneIndex, transform.position.y) * 10 * variations.Length;
+        int index = (int)rand / 10;
+
+        sr.sprite = variations[index-1];
+    }
+}
