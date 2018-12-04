26ab45adb62c37acdcb9dec5e645176e2265d09c
Joao Silva <joaosdev@gmail.com>
2018-12-04 00:45:42 +0000

    Fixed directionaltiles

--- Assets/Scripts/DirectionalTile.cs
+++ Assets/Scripts/DirectionalTile.cs
@@ -7,8 +7,12 @@ public class DirectionalTile : MonoBehaviour
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
-        {
-            colliders.transform.eulerAngles = new Vector3(0.0f, 0.0f, 180.0f);
-        }
+            colliders.transform.Rotate(0, 0, 180);
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            colliders.transform.Rotate(0, 0, 180);
     }
 }
